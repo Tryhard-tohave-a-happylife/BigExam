9887f9dff4c31093fc04234a37088c080b6bf0a9
18020778 <56502715+18020778@users.noreply.github.com>
2020-07-27 17:39:06 +0700

    addJQ

--- CareerWeb/Assets/Client/Js/Register/RegisterForEmployee.js
+++ CareerWeb/Assets/Client/Js/Register/RegisterForEmployee.js
@@ -1,1 +1,13 @@
-﻿
+﻿$(function () {
+
+    $("#button").click(function () {
+        var val = $('#item').val()
+        var xyz = $('#items option').filter(function () {
+            return this.value == val;
+        }).data('xyz');
+        var msg = xyz ? 'xyz=' + xyz : 'No Match';
+        alert(msg)
+
+    })
+
+})
